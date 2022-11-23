be86de6070a312a201cc21fa639301f95377efc5
ASMA BEN-ZINE <66072954+BENZINE-ASMA@users.noreply.github.com>
2022-11-23 02:33:39 +0100

    Add files via upload

---
+++ Joueur.java
@@ -0,0 +1,11 @@
+public class Joueur  extends Entite{
+    public Joueur() { resistance = 3; }
+    public Joueur(int r) { resistance = r; }
+    @Override
+    String toString(String background) {
+        return "H";
+    }
+
+    //still don't know what s his resistance
+    //if he s on sortie what to do 
+}
